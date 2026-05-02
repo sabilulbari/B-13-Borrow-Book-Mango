07be9cdfb13ee5a35113441a669bbd10320e3c61
sabilulbari <mdsabilulbari100@gmail.com>
2026-05-02 12:07:00 +0600

    added react icone modiul

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-
-
-import { FaXTwitter, FaGithub, FaLinkedin, FaDiscord, FaYoutube, FaInstagram, FaFacebook } from 'react-icons/fa6';
+import { FaXTwitter, FaInstagram, FaFacebook } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -14,7 +12,7 @@ const Footer = () => {
           <div className="md:col-span-4">
             <div className="flex items-center gap-3 mb-6">
               <Link href="/" className=" cursor-pointer text-3xl hover:border-none font-bold bg-linear-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
-                DigiTools
+                Digital Book
               </Link>
             </div>
 
