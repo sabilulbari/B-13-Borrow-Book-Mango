6bcca2e8ef1e7a3a58b590d7ae0ddb7c38f06ca4
sabilulbari <mdsabilulbari100@gmail.com>
2026-05-02 16:56:32 +0600

    Added Featured Book

--- src/app/components/feauredBooks/Book.jsx
+++ src/app/components/feauredBooks/Book.jsx
@@ -1,11 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { FaStar } from 'react-icons/fa6';
 
-const Book = () => {
-    return (
-        <div>
-            
+const Book = ({ book }) => {
+  const { image_url, title, category, author } = book;
+  return (
+    <div>
+      <div className="card bg-base-100 shadow-sm p-4 work-sans border border-gray-300">
+        <div className="bg-base-200 flex items-center rounded-xl justify-center p-4">
+          <Image className="h-50 rounded-lg" src={image_url} width={200} height={200} alt="Shoes" />
         </div>
-    );
+        <div className="card-body text-[#131313ca]">
+          <h2 className="card-title font-bold text-[24px] ">{title}</h2>
+          <p className="font-medium">By : {author}</p>
+          <div
+            className="flex items-center justify-between
+         border-t-2 border-dashed border-[#8d8a8a] pt-4  "
+          >
+            <div className="font-medium text-[14px]">Category: {category}</div>
+            <Link className='btn btn-error' href={'/bookDetails'}>View Details</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Book;
--- src/app/components/feauredBooks/FeaturedBooks.jsx
+++ src/app/components/feauredBooks/FeaturedBooks.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BooksData } from '../../../../lib/data';
 import Marquee from 'react-fast-marquee';
 import HeaderMarque from '../marque/HeaderMarque';
+import Book from './Book';
 
 const FeaturedBooks = async () => {
 
@@ -9,10 +10,15 @@ const FeaturedBooks = async () => {
     console.log(Books);
 
     return (
-      <div>
-        <HeaderMarque/>          
+      <div className='w-[90%] mx-auto py-15 space-y-10'>
+        <HeaderMarque />
         <div></div>
-        <h2>All: {Books.length}</h2>
+        <h2 className='text-center text-gray-600 text-4xl font-medium'>Featured Books</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {Books.slice(0, 4).map((book) => (
+            <Book key={book.id} book={book} />
+          ))}
+        </div>
       </div>
     );
 };
